72df4f8ff33b5640a10f700731060dff4469847c
Wedad <wedad.gamal@gmail.com>
2022-04-18 22:04:20 +0200

    Add project files.

---
+++ eTickects/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTickects.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
